79d32792c34a2d8e68467c308ebd551bfb125f50
Ravi Khatri <iravikumar92@gmail.com>
2023-08-06 03:31:41 +0530

    Added fonts

--- templates/src/components/Header/index.js
+++ templates/src/components/Header/index.js
@@ -2,27 +2,24 @@
 
 function Header() {
     return (
-        <header className="bg-gray-200">
-            <a className="" href="/">
-                <img src="./logo.svg" alt="Cabride Logo" />
-            </a>
-            <nav className="">
-                <div className="">
+        <header className="py-3  bg-blue-950">
+            <div className="xl:container mx-auto flex justify-between">
+                <a className="" href="/">
+                    <img className="h-10" src="./logo.svg" alt="Cabride Logo" />
+                </a>
+                <nav className="flex items-center text-white">
                     <div className="">
-                        <a href="/" className="">Home</a>
-                        <a href="/cabs-nearby" className="">Cabs Nearby</a>
-                        <a href="/past-rides" className="">Past Rides</a>
+                        <a href="/" className="py-3 px-4">Home</a>
+                        <a href="/cabs-nearby" className="py-3 px-4">Cabs Nearby</a>
+                        <a href="/past-rides" className="py-3 px-4">Past Rides</a>
+                        <a href="/logout" className="py-3 px-4">Logout</a>
                     </div>
-                    <div className="">
-                        <div className="">
-                            <div className="buttons">
-                                <a href="/" className="">Sign up</a>
-                                <a href="/" className="">Log in</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+                    {/* <div className="">
+                        <a href="/" className="py-3 px-4 mr-2">Log in</a>
+                        <a href="/" className="py-3 px-6 bg-blue-700 text-white rounded">Sign up</a>
+                    </div> */}
+                </nav>
+            </div>
         </header>
     )
 }
